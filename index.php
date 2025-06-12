59e1b74951442916ba86fc207e5a18339eddf1cc
tommyXD777 <yessidarias133@gmail.com>
2025-06-12 12:17:42 -0500

    ajuste de tamañao de los laterales

--- index.php
+++ index.php
@@ -23,14 +23,15 @@ $resultado = $conexion->query("SELECT * FROM servicios");
     <link rel="stylesheet" href="/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
-  .servicio-img {
+ .servicio-img {
     width: 100%;
-    height: 200px;
+    height: 250px;
     object-fit: cover;
     border-radius: 12px;
     margin-bottom: 10px;
     transition: transform 0.3s ease;
 }
+
 
 .servicio-img:hover {
     transform: scale(1.03);
