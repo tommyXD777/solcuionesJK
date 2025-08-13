76138e064613d18782a63f4feb489d0401d1e094
tommyXD777 <yessidarias133@gmail.com>
2025-08-13 15:20:41 -0500

    cambiode puerto

--- conexion.php
+++ conexion.php
@@ -1,11 +1,9 @@
 <?php
-// Obtener variables desde entorno Railway
-$host = getenv('MYSQLHOST');
-$user = getenv('MYSQLUSER');
-$password = getenv('MYSQLPASSWORD');
-$database = getenv('MYSQL_DATABASE');
-$port = getenv('MYSQLPORT');
-
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$database = getenv('DB_NAME');
+$port = getenv('DB_PORT');
 
 // Crear la conexión
 $conexion = new mysqli($host, $user, $password, $database, intval($port));
@@ -14,4 +12,6 @@ $conexion = new mysqli($host, $user, $password, $database, intval($port));
 if ($conexion->connect_error) {
     die("❌ Conexión fallida: " . $conexion->connect_error);
 }
+
+echo "✅ Conexión exitosa a la base de datos";
 ?>
